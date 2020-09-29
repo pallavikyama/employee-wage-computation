98170e0104e1cc0cba34887e41ef7d832dd88e3f
pallavikyama <kyamap98@gmail.com>
2020-09-29 09:55:12 +0530

    Modified:Modified usecase to multiple companies having different parameters

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,24 +3,10 @@ package employeeWageComputation;
 public class EmployeeWage {
 
 	// CONSTANTS
-	public static final int WAGE_PER_HOUR = 20;
-	public static final int MAX_WORKING_HOURS = 100;
-	public static final int WORKING_DAYS_PER_MONTH = 20;
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 
-	// VARIABLES
-	int empHoursPerDay = 0;
-	int empHoursPerMonth = 0;
-	int workingDays = 0;
-	int empWage = 0;
-	int totalEmpWage = 0;
-
-	public EmployeeWage() {
-		System.out.println("Welcome to EMPLOYEE WAGE COMPUTATION.\n");
-	}
-
-	public int empTime() {
+	public static int empTime() {
 
 		// EMPLOYEE WORKING HOURS PER DAY
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -38,21 +24,30 @@ public class EmployeeWage {
 		return hours;
 	}
 
-	public void wageCalculate() {
+	public static void wageCalculate(String company, int wagePerHour, int maxWorkingHours, int workingDaysPerMonth) {
+
+		// VARIABLES
+		int empHoursPerDay = 0, empHoursPerMonth = 0, workingDays = 0, empWage = 0, totalEmpWage = 0;
+
 		// EMPLOYEE MONTHLY WAGE COMPUTATION
-		while (empHoursPerMonth < MAX_WORKING_HOURS && workingDays < WORKING_DAYS_PER_MONTH) {
+		while (empHoursPerMonth < maxWorkingHours && workingDays < workingDaysPerMonth) {
 			empHoursPerDay = empTime();
 			empHoursPerMonth += empHoursPerDay;
 			workingDays++;
-			empWage = empHoursPerDay * WAGE_PER_HOUR;
+			empWage = empHoursPerDay * wagePerHour;
 			System.out.println("Day" + workingDays + "\tEmployeeWage: " + empWage);
 			totalEmpWage += empWage;
 		}
-		System.out.println("Monthly Wage for the Employee is: " + totalEmpWage);
+		System.out.println("Monthly Wage for the Employee in " + company + " is: " + totalEmpWage);
 	}
 
 	public static void main(String[] args) {
-		EmployeeWage emp1 = new EmployeeWage();
-		emp1.wageCalculate();
+		System.out.println("Welcome to EMPLOYEE WAGE COMPUTATION.\n");
+		System.out.println("==========================\nCOMPANY-1");
+		wageCalculate("D-Mart", 20, 100, 20);
+		System.out.println("==========================\nCOMPANY-2");
+		wageCalculate("Reliance", 40, 50, 25);
+		System.out.println("==========================\nCOMPANY-3");
+		wageCalculate("Big Basket", 30, 60, 24);
 	}
 }
