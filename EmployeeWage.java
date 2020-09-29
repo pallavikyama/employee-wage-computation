8727e9397d2d7695149607b82704f973b749066f
pallavikyama <kyamap98@gmail.com>
2020-09-29 09:43:02 +0530

    Added:Employee Attendance Check is added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,7 +1,19 @@
 package employeeWageComputation;
 
 public class EmployeeWage {
+
+	// CONSTANTS
+	public static final int IS_FULL_TIME = 1;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to EMPLOYEE WAGE COMPUTATION.");
+		System.out.println("Welcome to EMPLOYEE WAGE COMPUTATION.\n");
+
+		// EMPLOYEE ATTENDENCE CHECK
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		System.out.println("empCheck: " + empCheck);
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is PRESENT.");
+		else
+			System.out.println("Employee is ABSENT.");
 	}
 }
