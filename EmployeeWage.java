2df4927a4d1e0deb1e0380e26ff3cc2af9edd5b9
pallavikyama <kyamap98@gmail.com>
2020-09-29 09:50:38 +0530

    Modified:Employee Wage is converted to Monthly instead of Daily

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,6 +4,7 @@ public class EmployeeWage {
 
 	// CONSTANTS
 	public static final int WAGE_PER_HOUR = 20;
+	public static final int WORKING_DAYS_PER_MONTH = 20;
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 
@@ -13,20 +14,25 @@ public class EmployeeWage {
 		// VARIABLES
 		int empHoursPerDay = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 
-		// EMPLOYEE DAILY WAGE COMPUTATION
-		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-		switch (empCheck) {
-		case IS_FULL_TIME:
-			empHoursPerDay = 8;
-			break;
-		case IS_PART_TIME:
-			empHoursPerDay = 4;
-			break;
-		default:
-			empHoursPerDay = 0;
+		// EMPLOYEE MONTHLY WAGE COMPUTATION
+		for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+			case IS_FULL_TIME:
+				empHoursPerDay = 8;
+				break;
+			case IS_PART_TIME:
+				empHoursPerDay = 4;
+				break;
+			default:
+				empHoursPerDay = 0;
+			}
+			empWage = empHoursPerDay * WAGE_PER_HOUR;
+			System.out.println("Day" + day + "\tEmployeeWage: " + empWage);
+			totalEmpWage += empWage;
 		}
-		empWage = empHoursPerDay * WAGE_PER_HOUR;
-		System.out.println("Daily Wage for the Employee is:  " + empWage);
+		System.out.println("\nMonthly Wage for the Employee is: " + totalEmpWage);
 	}
 }
