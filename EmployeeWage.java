9f1c1563476d6e701ec452a04fe62c04c8123521
pallavikyama <kyamap98@gmail.com>
2020-09-29 09:47:32 +0530

    Added:Part time Employee Daily Wage Computation is added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,8 +3,9 @@ package employeeWageComputation;
 public class EmployeeWage {
 
 	// CONSTANTS
-	public static final int IS_FULL_TIME = 1;
 	public static final int WAGE_PER_HOUR = 20;
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to EMPLOYEE WAGE COMPUTATION.\n");
@@ -14,9 +15,11 @@ public class EmployeeWage {
 		int empWage = 0;
 
 		// EMPLOYEE DAILY WAGE COMPUTATION
-		int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 		if (empCheck == IS_FULL_TIME)
 			empHoursPerDay = 8;
+		else if (empCheck == IS_PART_TIME)
+			empHoursPerDay = 4;
 		else
 			empHoursPerDay = 0;
 		empWage = empHoursPerDay * WAGE_PER_HOUR;
