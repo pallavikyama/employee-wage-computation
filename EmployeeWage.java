3a9d958bba8227a59208bc4261e85f36a057701e
pallavikyama <kyamap98@gmail.com>
2020-09-29 09:59:14 +0530

    Modified:Total Employee Wage is saved for each company using instance variables

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,10 +3,21 @@ package employeeWageComputation;
 public class EmployeeWage {
 
 	// CONSTANTS
+	public String company;
+	public int wagePerHour;
+	public int maxWorkingHours;
+	public int workingDaysPerMonth;
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
 
-	public static int empTime() {
+	public EmployeeWage(String company, int wagePerHour, int maxWorkingHours, int workingDaysPerMonth) {
+		this.company = company;
+		this.wagePerHour = wagePerHour;
+		this.maxWorkingHours = maxWorkingHours;
+		this.workingDaysPerMonth = workingDaysPerMonth;
+	}
+
+	public int empTime() {
 
 		// EMPLOYEE WORKING HOURS PER DAY
 		int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -24,11 +35,10 @@ public class EmployeeWage {
 		return hours;
 	}
 
-	public static void wageCalculate(String company, int wagePerHour, int maxWorkingHours, int workingDaysPerMonth) {
+	public void wageCalculate() {
 
 		// VARIABLES
 		int empHoursPerDay = 0, empHoursPerMonth = 0, workingDays = 0, empWage = 0, totalEmpWage = 0;
-
 		// EMPLOYEE MONTHLY WAGE COMPUTATION
 		while (empHoursPerMonth < maxWorkingHours && workingDays < workingDaysPerMonth) {
 			empHoursPerDay = empTime();
@@ -43,11 +53,14 @@ public class EmployeeWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to EMPLOYEE WAGE COMPUTATION.\n");
+		EmployeeWage dmart = new EmployeeWage("D-Mart", 20, 100, 20);
+		EmployeeWage reliance = new EmployeeWage("Reliance", 40, 50, 25);
+		EmployeeWage bigbasket = new EmployeeWage("Big Basket", 30, 60, 24);
 		System.out.println("==========================\nCOMPANY-1");
-		wageCalculate("D-Mart", 20, 100, 20);
+		dmart.wageCalculate();
 		System.out.println("==========================\nCOMPANY-2");
-		wageCalculate("Reliance", 40, 50, 25);
+		reliance.wageCalculate();
 		System.out.println("==========================\nCOMPANY-3");
-		wageCalculate("Big Basket", 30, 60, 24);
+		bigbasket.wageCalculate();
 	}
 }
