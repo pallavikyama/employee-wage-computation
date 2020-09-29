d5b7c7c9364c0daee4e078a3b4a1504ea290c9db
pallavikyama <kyamap98@gmail.com>
2020-09-29 09:52:05 +0530

    Modified:Employee Wage is calculated till a condition of max working hours and days is reached for a month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,6 +4,7 @@ public class EmployeeWage {
 
 	// CONSTANTS
 	public static final int WAGE_PER_HOUR = 20;
+	public static final int MAX_WORKING_HOURS = 100;
 	public static final int WORKING_DAYS_PER_MONTH = 20;
 	public static final int IS_FULL_TIME = 1;
 	public static final int IS_PART_TIME = 2;
@@ -13,11 +14,13 @@ public class EmployeeWage {
 
 		// VARIABLES
 		int empHoursPerDay = 0;
+		int empHoursPerMonth = 0;
+		int workingDays = 0;
 		int empWage = 0;
 		int totalEmpWage = 0;
 
 		// EMPLOYEE MONTHLY WAGE COMPUTATION
-		for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+		while (empHoursPerMonth < MAX_WORKING_HOURS && workingDays < WORKING_DAYS_PER_MONTH) {
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case IS_FULL_TIME:
@@ -29,8 +32,10 @@ public class EmployeeWage {
 			default:
 				empHoursPerDay = 0;
 			}
+			empHoursPerMonth += empHoursPerDay;
+			workingDays++;
 			empWage = empHoursPerDay * WAGE_PER_HOUR;
-			System.out.println("Day" + day + "\tEmployeeWage: " + empWage);
+			System.out.println("Day" + workingDays + "\tEmployeeWage: " + empWage);
 			totalEmpWage += empWage;
 		}
 		System.out.println("\nMonthly Wage for the Employee is: " + totalEmpWage);
